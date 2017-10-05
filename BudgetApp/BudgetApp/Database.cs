85fb1805d8beb042417f8e6d8f9d05c3b71a15bf
STUDENTS\1795803 <1795803@johnabbottcollege.net>
2017-10-05 12:57:02 -0400

    class work

--- BudgetApp/BudgetApp/Database.cs
+++ BudgetApp/BudgetApp/Database.cs
@@ -39,30 +39,31 @@ namespace BudgetApp
         //    }
         //    return list;
         //}
-        //public void AddRecord(Record r)
-        //{
-        //    SqlCommand insertCommand = new SqlCommand("INSERT INTO Record (Name,Age,Height) VALUES (@Amount,@Date)", conn);
-        //    insertCommand.Parameters.Add(new SqlParameter("Amount", r._Amount));
-        //    insertCommand.Parameters.Add(new SqlParameter("Date", r._Date));
-        //    insertCommand.Parameters.Add(new SqlParameter("Height", height));
-        //    insertCommand.ExecuteNonQuery();
-        //}
-        //public void AddCategory(Record r)
-        //{
-        //    SqlCommand insertCommand = new SqlCommand("INSERT INTO Record (Name,Age,Height) VALUES (@Name,@Age,@Height)", conn);
-        //    insertCommand.Parameters.Add(new SqlParameter("Name", name));
-        //    insertCommand.Parameters.Add(new SqlParameter("Age", age));
-        //    insertCommand.Parameters.Add(new SqlParameter("Height", height));
-        //    insertCommand.ExecuteNonQuery();
-        //}
-        //public void AddTag(Record r)
-        //{
-        //    SqlCommand insertCommand = new SqlCommand("INSERT INTO Record (Name,Age,Height) VALUES (@Name,@Age,@Height)", conn);
-        //    insertCommand.Parameters.Add(new SqlParameter("Name", name));
-        //    insertCommand.Parameters.Add(new SqlParameter("Age", age));
-        //    insertCommand.Parameters.Add(new SqlParameter("Height", height));
-        //    insertCommand.ExecuteNonQuery();
-        //}
-    }
+        public void AddAccount(String AccountName, String AccountType, int AccountNumber, double Balance)
+        {
+            SqlCommand insertCommand = new SqlCommand("INSERT INTO Accounts (AccountName,AccountType,Balance,AccountNumber) VALUES (@Name,@Type,@Balance,@Number)", conn);
+            insertCommand.Parameters.Add(new SqlParameter("Number", AccountNumber));
+            insertCommand.Parameters.Add(new SqlParameter("Balance", Balance));
+            insertCommand.Parameters.Add(new SqlParameter("Type", AccountType));
+            insertCommand.Parameters.Add(new SqlParameter("Name", AccountName));
+            insertCommand.ExecuteNonQuery();
+        }
+    //public void AddCategory(Record r)
+    //{
+    //    SqlCommand insertCommand = new SqlCommand("INSERT INTO Record (Name,Age,Height) VALUES (@Name,@Age,@Height)", conn);
+    //    insertCommand.Parameters.Add(new SqlParameter("Name", name));
+    //    insertCommand.Parameters.Add(new SqlParameter("Age", age));
+    //    insertCommand.Parameters.Add(new SqlParameter("Height", height));
+    //    insertCommand.ExecuteNonQuery();
+    //}
+    //public void AddTag(Record r)
+    //{
+    //    SqlCommand insertCommand = new SqlCommand("INSERT INTO Record (Name,Age,Height) VALUES (@Name,@Age,@Height)", conn);
+    //    insertCommand.Parameters.Add(new SqlParameter("Name", name));
+    //    insertCommand.Parameters.Add(new SqlParameter("Age", age));
+    //    insertCommand.Parameters.Add(new SqlParameter("Height", height));
+    //    insertCommand.ExecuteNonQuery();
+    //}
+}
 }
 
