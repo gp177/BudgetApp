fd0cb274cf750b17c541e40778544216d0e28aff
BH210-13\ipd <email>
2017-10-05 10:47:52 -0400

    -- *GP* Added set, get

---
+++ BudgetApp/BudgetApp/Record.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetApp
+{
+    class Record
+    {
+        // fields encapsulated
+
+       // Records table
+        private int RecordId{get; set;}
+        private DateTime _Date { get; set; }
+        private double _Amount { get; set; }
+        // TODO: Add Document   
+   
+        // Accounts table
+        private int AccountId { get; set; }
+        private String _AccountType { get; set; }
+        private double _AccounNumber { get; set; }
+        private double _Balance { get; set; }
+
+        // Category table
+        private int CategoryId { get; set; }
+        private String _CategoryType { get; set; }
+
+        // Tag table
+        private int TagId { get; set; }
+        private String _Descrption { get; set; }
+
+        // InterTag able
+        private int InterId { get; set; }
+
+    }
+}
