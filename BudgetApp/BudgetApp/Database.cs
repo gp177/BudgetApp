083deb2759f9742652abb93dde8d331b46b69eff
STUDENTS\1795803 <1795803@johnabbottcollege.net>
2017-10-05 09:54:10 -0400

    database connection

---
+++ BudgetApp/BudgetApp/Database.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetApp
+{
+    class Database
+    {
+
+        private String CONN_STRING= @"Server=tcp:redaleks.database.windows.net,1433;Initial Catalog=BudgetAppDB;Persist Security Info=False;User ID={sqladmin};Password={AG1project};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+    }
+}
