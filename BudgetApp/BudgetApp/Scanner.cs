86931ba4083621201b52dee7ef4955c15ec19d4d
GP <Giorgioplescia1@gmail.com>
2017-10-13 21:56:08 -0400

    -- Scanner somewhat working better.

---
+++ BudgetApp/BudgetApp/Scanner.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WIA;
+
+namespace BudgetApp
+{
+    public partial class Scanners : Form
+    {
+        Device oDevice;
+        Item oItem;
+        CommonDialogClass dlg;
+
+        public Scanners()
+        {
+            InitializeComponent();
+            dlg = new CommonDialogClass();
+            oDevice = dlg.ShowSelectDevice(WiaDeviceType.ScannerDeviceType, true, false);
+        }
+        public void Scann()
+        {
+            dlg.ShowAcquisitionWizard(oDevice);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Scanners oScanner = new Scanners();
+                oScanner.Scann();
+                button1.Text = "Image scanned";
+                OpenFileDialog dlg = new OpenFileDialog();
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.Image = Image.FromFile(dlg.FileName);
+                }
+            } catch (Exception ex)
+            {
+                MessageBox.Show("Oops, something went wrong.");
+            }
+        }
+    }
+}
